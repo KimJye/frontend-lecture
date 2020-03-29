63b537d15892f2b53db9415c89496939f77b9d94
kimjye <eoeogudgud@naver.com>
2020-03-29 16:47:05 +0900

    feat: 과제

---
+++ hw/week2/split.js
@@ -0,0 +1,10 @@
+/**
+ * "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be."
+ * 위 문제의 문자열에서 공백을 기준으로 문자열을 쪼개서, 문자열 배열을 만들고 그 배열을 출력
+ */
+
+const str = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be.";
+
+const result = str.split(" ");
+
+console.log(result)
---
+++ hw/week2/vowel.js
@@ -0,0 +1,12 @@
+/**
+ * "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be."
+ * 위 문자열에서 모음(aeiou)들의 위치를 출력하는 코드를 구현하시오.
+ */
+
+const str = "When I find myself in times of trouble Mother Mary comes to me Speaking words of wisdom, let it be.";
+
+for (let i = 0; i < str.length; ++i) {
+     const alpha = str.charAt(i);
+     const regx = /[aeiou]/ig;
+     if(regx.test(alpha)) console.log(i);
+}
