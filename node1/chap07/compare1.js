77569b088fc0c64a454cb36d7021e72aad6bab5b
kimjye <eoeogudgud@naver.com>
2020-04-06 16:03:52 +0900

    feat: 객체생성과 객체 비교

---
+++ node1/chap07/compare1.js
@@ -0,0 +1,10 @@
+let person1 = { name: "홍길동", age: 16 }; 
+let person2 = { name: "홍길동", age: 16 };
+
+let p = person1; 
+
+console.log(person1 == person2); // false
+console.log(person1 == p); // true
+
+// 참조형인 객체를 == 연산자로 비교하면, 객체 내부 값을 비교하지 않고,
+// 두 참조를 비교한다
---
+++ node1/chap07/compare2.js
@@ -0,0 +1,7 @@
+let s1 = "hello world"; 
+let s2 = "hello " + "world"; 
+
+console.log(s1 == s2); // true
+
+// javascript에서 문자열은 기본 자료형이다.
+// 기본 자료형을 == 연산자로 비교하면, 값이 같은지를 비교한다.
