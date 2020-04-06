f6a54a51655c52742778b003fbb4ba67848167de
kimjye <eoeogudgud@naver.com>
2020-04-06 17:15:32 +0900

    feat: class

---
+++ node1/chap08/rectangle1.js
@@ -0,0 +1,18 @@
+class Rectangle { 
+    constructor(w, h) { 
+        this.width = w; 
+        this.height = h; 
+    } 
+    
+    area() { return this.width * this.height; } 
+} 
+
+let rectangle = new Rectangle(10, 15); 
+
+console.log(rectangle); 
+console.log(rectangle.area());
+
+/**
+ * Rectangle { width: 10, height: 15 }
+ * 150
+ */
